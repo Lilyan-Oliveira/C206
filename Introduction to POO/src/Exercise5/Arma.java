ef76b23feba3d3acabe7cf37c4c0d284eae5fe42
Lilyan Oliveira <lilyan.oliveira@ges.inatel.br>
2024-04-11 23:06:55 -0300

    Add files via upload
    Signed-off-by: Lilyan Oliveira <lilyan.oliveira@ges.inatel.br>

---
+++ Introduction to POO/src/Exercise5/Arma.java
@@ -0,0 +1,14 @@
+public class Arma {
+
+    String nome;
+    int poder;
+    int resistencia;
+    String descricao;
+
+    void mostraInfoArma(){
+
+        System.out.println("Descrição: " +descricao);
+        System.out.println("Resistencia: " +descricao);
+
+    }
+}
