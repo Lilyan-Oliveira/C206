cfb6f400998eb18d3a30dceb8fca80ae8b6481ba
Lilyan Oliveira <lilyan.oliveira@ges.inatel.br>
2024-04-11 23:47:58 -0300

    Códigos de introdução a POO
    Signed-off-by: Lilyan Oliveira <lilyan.oliveira@ges.inatel.br>

---
+++ Introduction to POO/src/Exercise6/Main.java
@@ -0,0 +1,42 @@
+package Exercise6;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Kart kart1 = new Kart();
+        Kart kart2 = new Kart();
+
+        kart1.nome = "Kart1";
+        kart2.nome = "Kart2";
+
+        kart1.motor.cilindradas = "150";
+        kart1.motor.velocidadeMaxima = 95.20f;
+
+        kart2.motor.cilindradas = "50";
+        kart2.motor.velocidadeMaxima = 76.10f;
+
+        //Criando dois pilotos
+        Piloto piloto1 = new Piloto();
+        Piloto piloto2 = new Piloto();
+
+        //Atribuindo valores para os pilotos
+        piloto1.nome = "Piloto1";
+        piloto2.nome = "Piloto2";
+
+        //Associando os pilotos aos Karts
+        kart1.piloto = piloto1;
+        kart2.piloto = piloto2;
+
+        //Executando açoes do piloto
+        piloto1.soltaSuperPoder();
+        piloto2.soltaSuperPoder();
+
+        //Executando acoes do Kart1
+        kart1.fazerDrift();
+        kart1.motor.mostraInfo();
+
+        //Executando acoes do Kart2
+        kart2.soltarTurbo();
+        kart2.motor.mostraInfo();
+    }
+}
---
+++ Introduction to POO/src/Exercise6/Piloto.java
@@ -0,0 +1,11 @@
+package Exercise6;
+
+public class Piloto {
+
+    String nome;
+    boolean vilao;
+
+    void soltaSuperPoder(){
+        System.out.println("Piloto " + nome + " esta soltando super poder.");
+    }
+}
